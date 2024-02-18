32ca9ff7cf28452909b18b61ffe85e280db7b896
zombkit <bakhmetstephen@gmail.com>
2024-02-18 15:03:27 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -64,55 +64,17 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     ..default()
                 },
             ),
-            TextSection::from_style(if cfg!(feature = "default_font") {
-                TextStyle {
-                    font_size: 60.0,
-                    color: Color::GOLD,
-                    // If no font is specified, the default font (a minimal subset of FiraMono) will be used.
-                    ..default()
-                }
-            } else {
-                // "default_font" feature is unavailable, load a font to use instead.
-                TextStyle {
-                    font: asset_server.load("fonts/DejaVuSansMono-Oblique.ttf"),
-                    font_size: 60.0,
-                    color: Color::GOLD,
-                }
+            TextSection::from_style( TextStyle {
+                font: asset_server.load("fonts/DejaVuSansMono-Oblique.ttf"),
+                font_size: 60.0,
+                color: Color::GOLD,
             }),
         ]),
         FpsText,
     ));
 
-    #[cfg(feature = "default_font")]
-    commands.spawn(
-        // Here we are able to call the `From` method instead of creating a new `TextSection`.
-        // This will use the default font (a minimal subset of FiraMono) and apply the default styling.
-        TextBundle::from("From an &str into a TextBundle with the default font!").with_style(
-            Style {
-                position_type: PositionType::Absolute,
-                bottom: Val::Px(5.0),
-                left: Val::Px(15.0),
-                ..default()
-            },
-        ),
-    );
 
-    #[cfg(not(feature = "default_font"))]
-    commands.spawn(
-        TextBundle::from_section(
-            "Default font disabled",
-            TextStyle {
-                font: asset_server.load("fonts/DejaVuSansMono-Oblique.ttf"),
-                ..default()
-            },
-        )
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            bottom: Val::Px(5.0),
-            left: Val::Px(15.0),
-            ..default()
-        }),
-    );
+  
 }
 
 fn text_color_system(time: Res<Time>, mut query: Query<&mut Text, With<ColorText>>) {
