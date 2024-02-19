088012c46abfdc43c3a634d2aa79ba7d033bec5e
zombkit <bakhmetstephen@gmail.com>
2024-02-18 18:16:15 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 //! in the bottom right. For text within a scene, please see the text2d example.
 
 use bevy::app::AppExit;
+use bevy::prelude::Color as BevyColor;
 use bevy::{
     diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
     prelude::*,
@@ -15,18 +16,48 @@ fn main() {
     App::new()
         .add_plugins((DefaultPlugins, FrameTimeDiagnosticsPlugin))
         .add_systems(Startup, setup)
-        .add_systems(Update, (text_update_system, text_color_system))
+    
         .add_systems(Update, keyboard_input)
         .run();
 }
 
 // A unit struct to help identify the FPS UI component, since there may be many Text components
 #[derive(Component)]
-struct FpsText;
+struct VirtualTerminal{
+    term_rows: u16,
+    term_columns: u16,
+    term_font_size: u16,
+    default_bg: BevyColor,
+    default_fg: BevyColor
+
+}
 
 // A unit struct to help identify the color-changing Text component
 #[derive(Component)]
-struct ColorText;
+struct VirtualCell{
+    fg: BevyColor,
+    bg: BevyColor,
+    underline_color: Option<BevyColor>,
+    skip:bool,
+    bold:bool,
+    dim:bool,
+    italic:bool,
+    underlined:bool,
+    slow_blink:bool,
+    rapid_blink:bool,
+    reversed:bool,
+    hidden:bool,
+    crossed_out:bool,
+
+}
+
+#[derive(Component)]
+struct VirtualCellPos{
+    x: u16,
+    y:u16
+
+
+}
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // UI camera
@@ -36,7 +67,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         // Create a TextBundle that has a Text with a single section.
         TextBundle::from_section(
             // Accepts a `String` or any type that converts into a `String`, such as `&str`
-            "hello\nbevy!",
+            "╬",
             TextStyle {
                 // This font is loaded and will be used instead of the default font.
                 font: asset_server.load("fonts/DejaVuSansMono-Oblique.ttf"),
@@ -48,63 +79,39 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         // Set the style of the TextBundle itself.
         .with_style(Style {
             position_type: PositionType::Absolute,
-            bottom: Val::Px(5.0),
-            right: Val::Px(5.0),
+            bottom: Val::Px(0.0),
+            left: Val::Px(0.0),
+            ..default()
+        }),
+        ColorText,
+    ));
+    commands.spawn((
+        // Create a TextBundle that has a Text with a single section.
+        TextBundle::from_section(
+            // Accepts a `String` or any type that converts into a `String`, such as `&str`
+            "╬",
+            TextStyle {
+                // This font is loaded and will be used instead of the default font.
+                font: asset_server.load("fonts/DejaVuSansMono-Oblique.ttf"),
+                font_size: 100.0,
+                ..default()
+            },
+        ) // Set the justification of the Text
+        .with_text_justify(JustifyText::Center)
+        // Set the style of the TextBundle itself.
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(0.0),
+            left: Val::Px(50.0),
             ..default()
         }),
         ColorText,
     ));
 
-    // Text with multiple sections
-    commands.spawn((
-        // Create a TextBundle that has a Text with a list of sections.
-        TextBundle::from_sections([
-            TextSection::new(
-                "FPS: ",
-                TextStyle {
-                    // This font is loaded and will be used instead of the default font.
-                    font: asset_server.load("fonts/DejaVuSansMono-Oblique.ttf"),
-                    font_size: 60.0,
-                    ..default()
-                },
-            ),
-            TextSection::from_style(TextStyle {
-                font: asset_server.load("fonts/DejaVuSansMono-Oblique.ttf"),
-                font_size: 60.0,
-                color: Color::GOLD,
-            }),
-        ]),
-        FpsText,
-    ));
+
 }
 
-fn text_color_system(time: Res<Time>, mut query: Query<&mut Text, With<ColorText>>) {
-    for mut text in &mut query {
-        let seconds = time.elapsed_seconds();
 
-        // Update the color of the first and only section.
-        text.sections[0].style.color = Color::Rgba {
-            red: (1.25 * seconds).sin() / 2.0 + 0.5,
-            green: (0.75 * seconds).sin() / 2.0 + 0.5,
-            blue: (0.50 * seconds).sin() / 2.0 + 0.5,
-            alpha: 1.0,
-        };
-    }
-}
-
-fn text_update_system(
-    diagnostics: Res<DiagnosticsStore>,
-    mut query: Query<&mut Text, With<FpsText>>,
-) {
-    for mut text in &mut query {
-        if let Some(fps) = diagnostics.get(&FrameTimeDiagnosticsPlugin::FPS) {
-            if let Some(value) = fps.smoothed() {
-                // Update the value of the second section
-                text.sections[1].value = format!("{value:.2}");
-            }
-        }
-    }
-}
 
 fn keyboard_input(keys: Res<ButtonInput<KeyCode>>, mut exit: EventWriter<AppExit>) {
     if keys.just_pressed(KeyCode::KeyQ) {
